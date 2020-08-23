4c32486423384b62312f34c810486e359ee635fd
yara265 <yaranuesoli@gmail.com>
2020-08-22 21:20:04 -0400

    tela

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void loginn(View view)
+    {
+        Intent intent = new Intent(this,telainicial.class);
+        startActivity(intent);
+    }
+}
